0d33fee4f3ff1b5501e8ef4b1400c8146105e7ae
hb717 <51805126+hb717@users.noreply.github.com>
2019-08-08 20:46:09 -0300

    cabeçalho e rodapé das páginas em php 08/0/2019

---
+++ footer.php
@@ -0,0 +1,72 @@
+
+
+
+      <!-- FOOTER -->
+	<footer class="container py-5">
+      <div class="row">
+        <div class="col-12 col-md">
+                    <h5>Contato</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">(21) 3460-0800</a></li>
+            <li><a class="text-muted" href="#">atendimento@quentnet.com.br</a></li>
+            <li><a class="text-muted" href="#">Av. Mal. Floriano, 6 - Centro, Rio de Janeiro - RJ, 20080-006</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Horários</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado De 11:00 às 23:30</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Informações</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Política de Privacidade</a></li>
+            <li><a class="text-muted" href="#">Termos de uso</a></li>
+            <li><a class="text-muted" href="#">Regiões de Entrega</a></li>
+			<li><a class="text-muted" href="#">Intranet</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Pagamento</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Cartão de Crédito</a></li>
+            <li><a class="text-muted" href="#">Cartão de Débito</a></li>
+            <li><a class="text-muted" href="#">Dinheiro</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Sobre</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Equipe</a></li>
+            <li><a class="text-muted" href="#">Locais</a></li>
+            <li><a class="text-muted" href="#">Privacidade</a></li>
+            <li><a class="text-muted" href="#">Termos</a></li>
+          </ul>
+        </div>
+		 <div class="col-6 col-md">
+          <h5>Sobre</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Equipe</a></li>
+            <li><a class="text-muted" href="#">Locais</a></li>
+            <li><a class="text-muted" href="#">Privacidade</a></li>
+            <li><a class="text-muted" href="#">Termos</a></li>
+          </ul>
+        </div>
+      </div>
+	  <hr class="featurette-divider">
+	  
+    </footer>
+    </main>
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="theme/jquery-slim.min.js"><\/script>')</script>
+    <script src="theme/popper.min.js"></script>
+    <script src="theme/bootstrap-4.1.3/dist/js/bootstrap.min.js"></script>
+    <!-- Só faz o nossos placeholders de imagens funcionarem. Não precisar copiar a próxima linha! -->
+    <script src="theme/holder.min.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,87 @@
+﻿<!-- ?php
+	/* require_once(inc/app.php); */
+	session_start();
+	if (!isset($_SESSION["email_logado"])) {
+		header("Location:login.php");
+	} else {
+		$email = $_SESSION["email_logado"];
+	}
+	
+? -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Quentnet</title>
+    <!-- Principal CSS do Bootstrap -->
+    <link href="theme/bootstrap-4.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="theme/carousel.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Quentnet</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExample02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+<div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="text-muted" href="#">Login/Cadastro</a>&nbsp&nbsp&nbsp&nbsp
+        <a class="btn btn-sm btn-outline-secondary" href="#"><img src="images/carrinho.png"></a>
+      </div>
+  </div>
+</nav>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-md-left" id="navbarsExample10">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Início<span class="sr-only">(atual)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">O Quentnet</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cardápio</a>
+            </li>
+            <!--<li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown10">
+                <a class="dropdown-item" href="#">Item</a>
+                <a class="dropdown-item" href="#">Outro item</a>
+                <a class="dropdown-item" href="#">Algum outro item</a>
+              </div>
+            </li>-->
+			<li class="nav-item">
+              <a class="nav-link" href="#">Áreas de entrega</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="contatos.php">Fale Conosco</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+	
+    <main role="main">
+
+
