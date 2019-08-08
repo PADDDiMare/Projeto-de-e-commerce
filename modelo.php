598d3b76b2b4b5425d19d59d1ab34ead378c1000
hb717 <51805126+hb717@users.noreply.github.com>
2019-08-08 20:49:54 -0300

    corpo de página modelo para inclusão de código em php - 08/08/2019
    Utilize esta página para incluir códigos em php

---
+++ modelo.php
@@ -0,0 +1,22 @@
+<?php 
+	session_start(); 
+// Chamando o cabeçalho do tema --> "header.php" //
+require ("header.php");	
+?>
+	
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <!-- div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Usuário</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group mr-2">
+            <a href="editar_usuario.php" class="btn btn-sm btn-outline-primary">Novo usuário</a>
+          </div>
+        </div>
+      </div -->
+	  
+	  
+	  </main>
+
+<?php 
+require ("footer.php");	
+?>
